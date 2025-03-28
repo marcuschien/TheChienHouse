fc413f3ef207cb5b5e78aa1a752e6e531f9350fc
Marcus Chien <mchien5@uwo.ca>
2025-03-27 18:01:38 -0700

    Create Sale Model
    Model for Sale requests. Line Item class is defined within the Sale Model

---
+++ LightspeedTakeHome/Models/Sale.cs
@@ -0,0 +1,17 @@
+﻿namespace LightspeedTakeHome.Models
+{
+    public class Sale
+    {
+        public long Id { get; set; }
+        public LineItem[] LineItems {get; set;}
+        public long[] LineItemTotals {get; set;}
+        public long Total {get; set;}
+
+    }
+    
+    public class LineItem
+    {
+        public long Id {get; set;}
+        public long Quantity {get; set;}
+    }
+}
