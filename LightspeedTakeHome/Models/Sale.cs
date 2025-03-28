9c2df4f868dc1a5efbd32e8c5918c41c1913453b
Marcus Chien <mchien5@uwo.ca>
2025-03-28 13:36:54 -0700

    Update Sale.cs
    Updating the Sale model to include ProductID and use of the Product class in the Line Item class. This allows us to extrapolate the price of the product from the DB which is used to calculate the line item total.

--- LightspeedTakeHome/Models/Sale.cs
+++ LightspeedTakeHome/Models/Sale.cs
@@ -3,15 +3,19 @@
     public class Sale
     {
         public long Id { get; set; }
-        public LineItem[] LineItems {get; set;}
-        public long[] LineItemTotals {get; set;}
+
+        //Making the following required because you can't have a sale without a LineItem. 
+        public required List<LineItem> LineItems {get; set;}
         public long Total {get; set;}
 
     }
     
     public class LineItem
     {
-        public long Id {get; set;}
-        public long Quantity {get; set;}
+        public long Id { get; set; }
+        public required long ProductId { get; set; } // Using this to pull the Product from the DB.
+        public required long Quantity {get; set;}
+        public Product? ProductForSale { get; set; } // This is a reference to the product that is being sold, used for name and price data.
+        public long TotalCost { get; set; }
     }
 }
