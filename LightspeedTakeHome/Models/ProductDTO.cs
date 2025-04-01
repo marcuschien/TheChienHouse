f4de0cbf2c77eee9d35b96d9ba88b42a3fe0d129
Marcus Chien <mchien5@uwo.ca>
2025-03-31 18:36:17 -0700

    Create Data Transfer Objects
    Creating DTOs to decouple service layer and database layer. Allows us to guarantee what we are getting in the request and responses to the db layer and client side. Also allows us to manipulate the payload as needed (can omit properties, make it smaller, make it more legible for client etc.)

---
+++ LightspeedTakeHome/Models/ProductDTO.cs
@@ -0,0 +1,26 @@
+﻿namespace LightspeedTakeHome.Models
+{
+    public class ProductDTO
+    {
+        /* I would probably make multiple records for each type of request and response, i.e. Product
+         * CreateRequest, ProductCreateResponse, ProductGetRequest, ProductGetResponse, etc. 
+         * But for the scope of this assignment, I've just kept it as one for simplicity. 
+         */
+        public record ProductCreateRequest(
+            string Name, // Can I make these required?
+            decimal Price
+        );
+
+        public record ProductGetRequest(
+            long? Id
+        );
+
+        public record ProductResponse(
+            long Id,
+            string Name,
+            decimal Price,
+            DateTime CreatedAt,
+            DateTime UpdatedAt
+        );
+    }
+}
---
+++ LightspeedTakeHome/Models/SaleDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace LightspeedTakeHome.Models
+{
+    public class SaleDTO
+    {
+        public record SaleCreateRequest(
+            List<LineItem> LineItems,
+            decimal? SaleDiscount
+        );
+
+        public record SaleCreateResponse(
+            long Id,
+            List<LineItem> LineItems,
+            decimal Total,
+            decimal? Discount,
+            DateTime CreatedAt,
+            DateTime UpdatedAt
+        );
+    }
+}
