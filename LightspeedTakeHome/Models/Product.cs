8568ab6c9a265744f203bdb82dd91fef82a77b13
Marcus Chien <mchien5@uwo.ca>
2025-03-31 18:50:16 -0700

    Update Models
    Adding comments, data validation for some parameters, and timestamps.

--- LightspeedTakeHome/Models/Product.cs
+++ LightspeedTakeHome/Models/Product.cs
@@ -1,9 +1,19 @@
-﻿namespace LightspeedTakeHome.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LightspeedTakeHome.Models
 {
     public class Product
     {
         public long Id { get; set; }
-        public string? Name { get; set; }
-        public double Price { get; set; }
+
+        [Required(ErrorMessage = "Product name is required")]
+        [StringLength(100, ErrorMessage = "Product name cannot exceed 100 characters")]
+        public string? Name { get; set; } = string.Empty;
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0")]
+        public decimal Price { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow; // The timestamps are useful for tracing and debugging purposes 
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow; // Some customers find them useful as well
     }
 }
