16f476ca0814f14842c2d6a68f932be0400c9e84
Marcus Chien <mchien5@uwo.ca>
2025-03-31 18:34:37 -0700

    Update Controllers to Use Service Layer
    Adding service layer classes to controllers and calling the service layer for desired functionality instead of the db directly

--- LightspeedTakeHome/Controllers/ProductsController.cs
+++ LightspeedTakeHome/Controllers/ProductsController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using LightspeedTakeHome.Models;
+using LightspeedTakeHome.Services;
+using static LightspeedTakeHome.Models.ProductDTO;
 
 namespace LightspeedTakeHome.Controllers
 {
@@ -14,94 +16,37 @@ namespace LightspeedTakeHome.Controllers
     public class ProductsController : ControllerBase
     {
         private readonly RetailContext _context; // Using in memory database
+        private readonly IProductService _productService; // Utilizing the service layer to fetch requested data
 
-        public ProductsController(RetailContext context)
+        public ProductsController(RetailContext context, IProductService productService)
         {
             _context = context;
+            _productService = productService;
         }
 
         // GET: api/Products
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
         {
-            return await _context.Products.ToListAsync();
+            IEnumerable<ProductResponse> response = await _productService.GetProductsAsync();
+            return CreatedAtAction(nameof(GetProducts), response);
         }
 
         // GET: api/Products/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(long id)
         {
-            var product = await _context.Products.FindAsync(id);
-
-            if (product == null)
-            {
-                return NotFound();
-            }
-
-            return product;
-        }
-
-        // PUT: api/Products/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutProduct(long id, Product product)
-        {
-            if (id != product.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(product).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ProductExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
+            ProductResponse? response = await _productService.GetProductByIdAsync(id);
+            return CreatedAtAction(nameof(GetProduct),response); // Response could possibly be null, need to handle this case. 
         }
 
         // POST: api/Products
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Product>> PostProduct(Product product)
+        public async Task<ActionResult<Product>> PostProduct(ProductCreateRequest request)
         {
-            _context.Products.Add(product);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product); // This needs to return the line items in the sale, the line item totals, and the sale total.
-        }
-
-        // DELETE: api/Products/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteProduct(long id)
-        {
-            var product = await _context.Products.FindAsync(id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-
-            _context.Products.Remove(product);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool ProductExists(long id)
-        {
-            return _context.Products.Any(e => e.Id == id);
+            ProductResponse response = await _productService.CreateProductAsync(request);
+            return CreatedAtAction(nameof(PostProduct), new { id = response.Id }, response);
         }
     }
 }
