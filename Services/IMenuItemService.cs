698b2646fc79d077a91e54849e6d85a613a0cce3
Marcus Chien <marcuschien22@gmail.com>
2025-06-27 16:05:29 -0700

    Add POST method for menu items

--- Services/IMenuItemService.cs
+++ Services/IMenuItemService.cs
@@ -8,5 +8,6 @@ namespace TheChienHouse.Services
         Task<IEnumerable<MenuItemResponse>> GetMenuItemsAsync(DishName? dishName = null, DishType? dishType = null); // TODO: Introduce parameters int page and int pagesize to allow for pagination
         Task<MenuItemResponse> CreateMenuItemAsync(MenuItemCreateRequest request);
         Task<MenuItemResponse?> GetMenuItemByIdAsync(long id);
+        Task<MenuItemResponse> UpdateMenuItemAsync(MenuItemUpdateRequest request);
     }
 }
