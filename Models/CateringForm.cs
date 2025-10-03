a4ab251877a91c3e2df7cd59994041b1311ec36f
Marcus Chien <marcuschien22@gmail.com>
2025-10-03 12:34:17 -0700

    Create Catering Form Model and DTOs

---
+++ Models/CateringForm.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheChienHouse.Models
+{
+    public class CateringForm
+    {
+        public Guid Id { get; set; }
+
+        public CateringType CateringType { get; set; }
+
+        public List<DietaryRestrictions> MenuItems { get; set; } = new List<DietaryRestrictions>();
+
+        public Guid? ClientId { get; set; }
+
+        public DateTime EventDate { get; set; }
+
+        [Required(ErrorMessage = "Contact name is required")]
+        public required string ContactName { get; set; }
+
+        public string? ContactEmail { get; set; }
+
+        public string? ContactPhoneNumber { get; set; }
+
+    }
+
+    public enum DietaryRestrictions
+    {
+        Peanuts,
+        TreeNuts,
+        Dairy,
+        Fish,
+        Vegetarian,
+        Vegan,
+        GlutenFree,
+        Halal,
+        Kosher,
+        Shellfish,
+        Soy,
+        Egg,
+        None
+    }
+    public enum CateringType
+    {
+        Onsite,
+        Offsite,
+        Pickup,
+        Delivery,
+    }
+
+    public enum Status
+    {
+        Pending,
+        Confirmed,
+        Completed,
+        Cancelled
+    }
+}
