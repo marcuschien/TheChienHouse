87e063fbbe025c0c0f6fe48dd0f3cb9883ccf788
Marcus Chien <marcuschien22@gmail.com>
2025-10-03 17:07:38 -0700

    Create CateringFormService.cs

---
+++ Services/CateringFormService.cs
@@ -0,0 +1,42 @@
+﻿using TheChienHouse.Models;
+
+namespace TheChienHouse.Services
+{
+    public class CateringFormService : ICateringFormService
+    {
+        public Task<CateringFormDTO.CateringFormCreateResponse> CreateCateringFormAsync(CateringFormDTO.CateringFormCreateRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteCateringFormAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CateringForm?> GetCateringFormByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<CateringForm>> GetCateringFormsByClientIdAsync(Guid clientId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<CateringForm>> GetCateringFormsByDateRangeAsync(DateTime startDate, DateTime endDate, Guid? clientId = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<CateringForm>> GetCateringFormsByStatusAsync(Status status, Guid? clientId = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CateringFormDTO.CateringFormCreateResponse?> UpdateCateringFormAsync(CateringFormDTO.CateringFormCreateRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
