5d0a7829940837b12dacb974b422f3ba820b184b
Marcus Chien <marcuschien22@gmail.com>
2025-10-07 16:42:20 -0700

    Update CateringFormDTO.cs

--- Models/CateringFormDTO.cs
+++ Models/CateringFormDTO.cs
@@ -5,45 +5,31 @@ namespace TheChienHouse.Models
     public class CateringFormDTO
     {
         public record CateringFormCreateRequest
-        {
-            public Guid Id { get; set; }
-            public CateringType CateringType { get; set; }
-            public List<DietaryRestrictions> MenuItems { get; set; } = new List<DietaryRestrictions>();
-            public Guid? ClientId { get; set; }
-            [Required(ErrorMessage = "Event date is required")]
-            public DateTime EventDate { get; set; }
-            [Required(ErrorMessage = "Name is required")]
-            public required string ClientName { get; set; }
-            [Required(ErrorMessage = "Email is required")]
-            [EmailAddress(ErrorMessage = "Invalid email address")]
-            public string? ClientEmail { get; set; }
-            [Required(ErrorMessage = "Phone number is required")]
-            [Phone(ErrorMessage = "Invalid phone number")]
-            public string? ClientPhoneNumber { get; set; }
-            public Status Status { get; set; } = Status.Pending;
-            public DateTime CreatedAt { get; init; }
-            public DateTime? UpdatedAt { get; init; }
-
-        }
-        public record CateringFormCreateResponse
-        {
-            public Guid Id { get; set; }
-            public CateringType CateringType { get; set; }
-            public List<DietaryRestrictions> MenuItems { get; set; } = new List<DietaryRestrictions>();
-            public Guid? ClientId { get; set; }
-            [Required(ErrorMessage = "Event date is required")]
-            public DateTime EventDate { get; set; }
-            [Required(ErrorMessage = "Name is required")]
-            public required string ClientName { get; set; }
-            [Required(ErrorMessage = "Email is required")]
-            [EmailAddress(ErrorMessage = "Invalid email address")]
-            public string? ClientEmail { get; set; }
-            [Required(ErrorMessage = "Phone number is required")]
-            [Phone(ErrorMessage = "Invalid phone number")]
-            public string? ClientPhoneNumber { get; set; }
-            public Status Status { get; set; } = Status.Pending;
-            public DateTime CreatedAt { get; init; }
-            public DateTime? UpdatedAt { get; init; }
-        }
+        (
+            Guid Id,
+            CateringType CateringType,
+            List<DietaryRestrictions> DietaryRestrictions,
+            Guid? ClientId,
+            DateTime EventDate,
+            string ClientName,
+            string? ClientEmail,
+            string? ClientPhoneNumber,
+            Status Status,
+            DateTime CreatedAt,
+            DateTime? UpdatedAt
+        );
+        public record CateringFormCreateResponse( // These will be used as part of the success confirmation pop up after submission.
+            Guid Id,
+            CateringType CateringType,
+            List<DietaryRestrictions> DietaryRestrictions,
+            Guid? ClientId,
+            DateTime EventDate,
+            string ClientName,
+            string? ClientEmail,
+            string? ClientPhoneNumber,
+            Status Status,
+            DateTime CreatedAt,
+            DateTime? UpdatedAt
+        );
     }
 }
