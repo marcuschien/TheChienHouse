0a52ca50af66b277430ec04ffd35a94a0ccb1f01
Marcus Chien <marcuschien22@gmail.com>
2025-11-10 16:39:55 -0800

    Update LineItemDTO.cs

--- Models/LineItemDTO.cs
+++ Models/LineItemDTO.cs
@@ -4,14 +4,11 @@ using TheChienHouse.Models;
 public class LineItemDTO
 {
     public record LineItemCreateRequest
-    {
-        [Required(ErrorMessage = "Item name is required")]
-        public required Guid MenuItemForSale { get; set; }
-
-        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be greater than 0")]
-        public required int Quantity { get; set; }
-        public decimal Discount { get; set; } = 0; // This is x percent off.
-    }
+    (
+        Guid MenuItemForSale,
+        int Quantity,
+        decimal Discount // This is x percent off.
+    );
 
     public record LineItemCreateResponse(
         Guid Id,
